309392bd863e70465c82802f7576f656be6d7c6b
Nala Ayaba <joaovictor070102@gmail.com>
2023-09-01 17:38:51 -0300

    animação na palavra senhas

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+// Elemento que contém a palavra "senhas_" para o efeito de digitação
+const senhaText = document.getElementById("senha-text");
+
+// Texto digitado
+const textoParaDigitar = "******";
+
+// Velocidade da digitação (em milissegundos por caractere)
+const velocidadeDigitar = 230;
+
+// Tempo para exibir a palavra "senhas" após a animação
+const tempoParaExibirSenhas = 3000; // 3000 milissegundos = 3 segundos
+
+let caractereAtual = 0;
+
+function digitarCaractere() {
+  if (caractereAtual < textoParaDigitar.length) {
+    senhaText.textContent += textoParaDigitar.charAt(caractereAtual);
+    caractereAtual++;
+    setTimeout(digitarCaractere, velocidadeDigitar);
+  } else {
+    // Após a animação, exiba gradualmente a palavra completa "senhas"
+    senhaText.style.opacity = "0"; // Comece com a opacidade zero
+    senhaText.style.transition = "opacity 1s"; // Adicione uma transição suave
+    setTimeout(function () {
+      senhaText.textContent = "senhas";
+      senhaText.style.opacity = "1"; // Defina a opacidade de volta a 1 gradualmente
+    }, tempoParaExibirSenhas);
+  }
+}
+
+// Iniciar o efeito de digitação quando a página carregar
+window.onload = function () {
+  digitarCaractere();
+};
+
 document.getElementById("gerar-btn").addEventListener("click", gerarSenha); // Evento de clique para o botão "Gerar Senha"
 
-document.getElementById("password-form").addEventListener("submit", function(event) {
+document.getElementById("password-form").addEventListener("submit", function (event) {
   event.preventDefault(); // Evita o comportamento padrão de recarregar a página
   gerarSenha();
 });
@@ -22,9 +57,9 @@ function gerarSenha() {
   }
 
   if (!letrasRepetidas) { // Embaralha a senha para evitar repetições
-    senha = senha.split('').sort(function(){return 0.5-Math.random()}).join('');
+    senha = senha.split('').sort(function () { return 0.5 - Math.random() }).join('');
   }
 
   const senhaGeradaElement = document.getElementById("senha-gerada");
   senhaGeradaElement.textContent = "Sua senha gerada é: " + senha;
-};
+}
